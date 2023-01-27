4b1f2ce75ef7377b50f5af88a228fefb0709738e
Ridwan Annafi-RPL-12 <98511080+RidwanAnnV@users.noreply.github.com>
2023-01-27 09:14:29 +0700

    Add files via upload

--- latihan/dompdf.php
+++ latihan/dompdf.php
@@ -1,16 +1,35 @@
 <?php
 
-require_once './vendor/autoload.php';
+require_once './../vendor/autoload.php';
 
 
 $faker = Faker\Factory ::create();
+
 use Dompdf\Dompdf;
 
+$html="
+    <table>
+    <thead>
+    <th>NO</th>
+    <th>NAME</th>
+    <th>EMAIL</th>
+    <th>ADDRESS</th>
+    </thead>
+        <tbody>   
+";
+for ($i = 1; $i < 99; $i++){
+
+    $html .="<tr>";
+    $html .="<td>" . $i."</td>";
+    $html .="<td>" . $faker->name()."</td>";
+    $html .="<td>" . $faker->email()."</td>";
+    $html .="<td>" . $faker->address()."</td>";
+    $html .="</tr>";
+}
 
 $dompdf = new Dompdf();
-
-$html = "INI COMPOSTER <h1> GG </h1>";
 $dompdf->loadHtml($html);
+$html = "INI COMPOSTER <h1> GG </h1>";
 $dompdf->setPaper('A4','landscape');
 $dompdf->render();
 $dompdf->stream();
